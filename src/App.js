5244df0371b21d40c3282c926e444f0bd36393d2
iamai <youngdelta@gmail.com>
2026-01-03 09:50:05 +0900

    feat: introduce a basic To-Do list application with task creation and update core dependencies.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
 // import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import TodoRow from "./component/TodoRow";
 import TodoCreator from "./component/TodoCreator";
 import TodoBanner from "./component/TodoBanner";
-import VisibilityControl, {VisillityControl} from "./component/VisibilityControl"
+import VisibilityControl from "./component/VisibilityControl";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            userName: 'Adam',
-            todoItems: [
-                {action: 'Buy Flowers', done: false},
-                {action: 'Get Shoes', done: false},
-                {action: 'Collect Tickets', done: true},
-                {action: 'Call Joe', done: false},
-            ],
-            // newItemText: '',
-            showCompleted: true,
-        }
+    this.state = {
+      userName: "Adam",
+      todoItems: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false },
+      ],
+      // newItemText: '',
+      showCompleted: true,
+    };
+  }
 
+  /**
+   *
+   */
+  createNewTextTodo = () => {
+    if (
+      !this.state.todoItems.find(
+        (item) => item.action === this.state.newItemText
+      )
+    ) {
+      this.setState({
+        todoItems: [
+          ...this.state.todoItems,
+          { action: this.state.newItemText, done: false },
+        ],
+        newItemText: "",
+      });
     }
+  };
 
-    /**
-     *
-     */
-    createNewTextTodo = () => {
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Bob" : "Adam",
+    });
+  };
 
-        if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
-            this.setState({
-                todoItems: [...this.state.todoItems,
-                    {action: this.state.newItemText, done: false}
-                ],
-                newItemText: '',
-            })
-        }
-    }
+  /**
+   *
+   */
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+    // console.log(event.target.value)
+  };
 
-    changeStateData = () => {
-        this.setState({
-            userName: this.state.userName === 'Adam' ? 'Bob' : 'Adam',
-        })
-    }
-
-    /**
-     *
-     */
-    updateNewTextValue = (event) => {
-        this.setState({newItemText: event.target.value});
-        // console.log(event.target.value)
-    }
-
-    /**
-     *
-     */
-    /*
+  /**
+   *
+   */
+  /*
     createNewTodo = () => {
         if (!this.state.newItemText) return;
         if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
@@ -71,54 +74,80 @@ class App extends Component {
     }
 */
 
-    createNewTodo = (task) => {
-        if (!this.state.todoItems.find(item => item.action === task)) {
-            this.setState({
-                todoItems: [...this.state.todoItems, {action: task, done: false}]
-            }, () => localStorage.setItem("todos", JSON.stringify(this.state)));
-        }
+  createNewTodo = (task) => {
+    if (
+      task &&
+      task.trim().length > 0 &&
+      !this.state.todoItems.find((item) => item.action === task.trim())
+    ) {
+      this.setState(
+        {
+          todoItems: [
+            ...this.state.todoItems,
+            { action: task.trim(), done: false },
+          ],
+        },
+        () => localStorage.setItem("todos", JSON.stringify(this.state))
+      );
     }
+  };
 
-    toggleTodo = (todo) => this.setState({
-        todoItems: this.state.todoItems.map(item => item.action === todo.action ? {...item, done: !item.done} : item)
+  toggleTodo = (todo) =>
+    this.setState({
+      todoItems: this.state.todoItems.map((item) =>
+        item.action === todo.action ? { ...item, done: !item.done } : item
+      ),
     });
 
-    todoTableRows = (doneValue) => this.state.todoItems.filter(item => item.done === doneValue).map(item =>
-        <TodoRow key={item.action} item={item} callback={this.toggleTodo}/>
-    )
-    /*
+  todoTableRows = (doneValue) =>
+    this.state.todoItems
+      .filter((item) => item.done === doneValue)
+      .map((item) => (
+        <TodoRow key={item.action} item={item} callback={this.toggleTodo} />
+      ));
+  /*
         todoTableRows = () => this.state.todoItems.map(item =>
             <TodoRow key={item.action} item={item} callback={this.toggleTodo} />
         )
     */
 
-    componentDidMount = () => {
-        let data = localStorage.getItem('todos');
-        this.setState(data != null ? JSON.parse(data) : {
-            userName: 'Adam',
-            todoItems: [
-                {action: 'Buy Flowers', done: false},
-                {action: 'Get Shoes', done: false},
-                {action: 'Collect Tickets', done: true},
-                {action: 'Call Joe', done: false},
-            ],
-            showCompleted: true,
-        });
-
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos");
+    if (data != null) {
+      try {
+        const parsedData = JSON.parse(data);
+        if (parsedData && Array.isArray(parsedData.todoItems)) {
+          this.setState(parsedData);
+          return;
+        }
+      } catch (e) {
+        console.error("Failed to parse todos from localStorage", e);
+      }
     }
 
-    render = () => (
+    this.setState({
+      userName: "Adam",
+      todoItems: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false },
+      ],
+      showCompleted: true,
+    });
+  };
 
-        <div>
-            {/*
+  render = () => (
+    <div>
+      {/*
         <h4 className="bg-primary text-white text-center p-2">
             {this.state.userName} To Do List
             ({this.state.todoItems.filter(t => !t.donne).length} items to do)
         </h4>
       */}
-            <TodoBanner name={this.state.userName} tasks={this.state.todoItems}/>
-            <div className="container-fluid">
-                {/*
+      <TodoBanner name={this.state.userName} tasks={this.state.todoItems} />
+      <div className="container-fluid">
+        {/*
             <div className="my-1">
                 <input className="form-control" value={this.state.newItemText} onChange={this.updateNewTextValue} />
                 <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>
@@ -126,39 +155,40 @@ class App extends Component {
                 </button>
             </div>
         */}
-                <TodoCreator callback={this.createNewTodo}/>
-            </div>
-            {/*<button className="btn btn-primary m-2" onClick={this.changeStateData}>Change</button>*/}
-            <table className="table table-striped table-bordered">
-                <thead>
-                <tr>
-                    <th>Description</th>
-                    <th>Done</th>
-                </tr>
-                </thead>
-                <tbody>{this.todoTableRows(false)}</tbody>
-            </table>
-            <div className="bg-secondary text-white text-center p-2">
-                <VisibilityControl description="Completed Tasks" isChecked={this.state.showCompleted}
-                                   callback={(checked) => this.setState({showCompleted: checked})}/>
-            </div>
+        <TodoCreator callback={this.createNewTodo} />
+      </div>
+      {/*<button className="btn btn-primary m-2" onClick={this.changeStateData}>Change</button>*/}
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>{this.todoTableRows(false)}</tbody>
+      </table>
+      <div className="bg-secondary text-white text-center p-2">
+        <VisibilityControl
+          description="Completed Tasks"
+          isChecked={this.state.showCompleted}
+          callback={(checked) => this.setState({ showCompleted: checked })}
+        />
+      </div>
 
-            {
-                this.state.showCompleted &&
-                <table className={"table table-striped table-bordered"}>
-                    <thead>
-                    <tr>
-                        <ht>Description</ht>
-                        <th>Done</th>
-                    </tr>
-                    </thead>
-                    <tbody>{this.todoTableRows(true)}</tbody>
-                </table>
-            }
+      {this.state.showCompleted && (
+        <table className={"table table-striped table-bordered"}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoTableRows(true)}</tbody>
+        </table>
+      )}
+    </div>
 
-        </div>
-
-        /*
+    /*
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -176,8 +206,7 @@ class App extends Component {
           </header>
         </div>
         */
-    )
-
+  );
 }
 
 export default App;
--- src/component/TodoCreator.js
+++ src/component/TodoCreator.js
@@ -1,34 +1,40 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class TodoCreator extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            newItemText: '',
-        };
+    this.state = {
+      newItemText: "",
+    };
+  }
 
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  };
+
+  createNewTodo = () => {
+    if (this.state.newItemText && this.state.newItemText.trim().length > 0) {
+      this.props.callback(this.state.newItemText.trim());
+      this.setState({ newItemText: "" });
     }
+  };
 
-    updateNewTextValue = (event) => {
-        this.setState({newItemText: event.target.value});
-    }
-
-    createNewTodo = () => {
-        this.props.callback(this.state.newItemText);
-        this.setState({newItemText: ''});
-    }
-
-    render() {
-        return (
-            <div className="my-1">
-                <input className="form-control" value={this.state.newItemText} onChange={this.updateNewTextValue} />
-                <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>ADD</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="my-1">
+        <input
+          className="form-control"
+          value={this.state.newItemText}
+          onChange={this.updateNewTextValue}
+        />
+        <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>
+          ADD
+        </button>
+      </div>
+    );
+  }
 }
 
 export default TodoCreator;
